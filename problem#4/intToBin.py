2c913a1133940f14ad260561154a26b6442aafbb
Jacob Oliver <bjo8656@utulsa.edu>
2024-02-22 10:28:53 -0600

    lhljlh

--- problem#4/intToBin.py
+++ problem#4/intToBin.py
@@ -3,20 +3,27 @@ def int_to_reverse_binary(num1):
 #write your while loop here
     new = num1
     while (num1 > 0.99999):
+     
         new = (num1 % 2)
         binary_val.append(str(new))
         num1 = int(num1/2)
+    com = ""
+    binary_val = com.join(binary_val)
 
     return binary_val
 
 
 def string_reverse(input_string): 
-    reverse_input = input_string [::1]
+    #reverse_input = input_string [::1]
+    """"
+    reverse_input = input_string.split()
+    reverse_input = reverse_input [::1]
     sep = ""
     final= sep.join(reverse_input)
     
    #write your for loop here
-    
+    """
+    final  = input_string [::-1] 
     return final
 
 if __name__ == '__main__':
